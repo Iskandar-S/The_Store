221bfe0e23f7cfe096e7a24aa8d77ece67eef408
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-08-17 07:38:54 -0400

    fixed minor bugs

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -6,48 +6,36 @@ const dataController = require('./dataController');
 // add routes
 
 // Seed
-router.get(
-  'products/seed',
-  dataController.seed,
-  viewController.redirectHome,
-);
+router.get('/seed', dataController.seed, viewController.redirectHome);
 
 //  Index
-router.get('/products', dataController.index, viewController.index);
+router.get('/', dataController.index, viewController.index);
 
 // New
-router.get('/products/new', viewController.new);
+router.get('/new', viewController.new);
 
 // Delete
 router.delete(
-  '/products/:id',
+  '/:id',
   dataController.delete,
   viewController.redirectHome,
 );
 
 // Update
 router.put(
-  'products/:id',
+  '/:id',
   dataController.update,
   viewController.redirectShow,
 );
 
 // Create
-router.post(
-  'products/',
-  dataController.create,
-  viewController.redirectHome,
-);
+router.post('/', dataController.create, viewController.redirectHome);
 
 // Edit
-router.get(
-  'products/:id/edit',
-  dataController.show,
-  viewController.edit,
-);
+router.get('/:id/edit', dataController.show, viewController.edit);
 
 // Show
-router.get('products/:id', dataController.show, viewController.show);
+router.get('/:id', dataController.show, viewController.show);
 
 // export router
 module.exports = router;
