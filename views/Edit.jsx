b70b135163c9369735d815c5bd9f5146efcde9fd
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-08-17 06:24:14 -0400

    Added navbar and forms

--- views/Edit.jsx
+++ views/Edit.jsx
@@ -3,33 +3,134 @@ const React = require('react');
 class Edit extends React.Component {
   render() {
     const { product } = this.props;
-    <html>
-      <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        />
-        <title>${product.name}</title>
-        <link rel="stylesheet" href="/css/app.css" />
-      </head>
-      <body>
-        <ul>
-          {products.map((product) => {
-            return (
-              <li key={product.id}>
-                <a href={`/products/${product.id}`}>
-                  <img
-                    src={`${product.img}`}
-                    alt={`${product.name}`}
-                  />
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </body>
-    </html>;
+    return (
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>Edit Product</title>
+          <script
+            src="https://kit.fontawesome.com/2f4558e6ee.js"
+            crossorigin="anonymous"
+          ></script>
+          <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+            crossorigin="anonymous"
+          ></link>
+          <link rel="stylesheet" href="/css/app.css" />
+        </head>
+        <body>
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="/products">
+                <img
+                  src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                  alt=""
+                  width="90"
+                  height="24"
+                  class="d-inline-block align-text-top"
+                />
+                <span> but from </span>
+                <img
+                  src="https://1000logos.net/wp-content/uploads/2021/05/Wish-logo.png"
+                  alt=""
+                  width="85"
+                  height="24"
+                  class="d-inline-block align-text-top"
+                />
+              </a>
+            </div>
+          </nav>
+          <div class="container mt-5">
+            <form
+              action={`/products/${product.id}?_method=PUT`}
+              method="POST"
+            >
+              <div class="mb-3">
+                <label for="InputProductTitle" class="form-label">
+                  Product title
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  name="productTitle"
+                  defaultValue={product.productTitle}
+                />
+              </div>
+              <div class="mb-3">
+                <label for="InputManufacturer" class="form-label">
+                  Manufacturer/Brand
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  name="manufacturer"
+                  defaultValue={product.manufacturer}
+                />
+              </div>
+              <div class="mb-3">
+                <label for="InputQuantity" class="form-label">
+                  Quantity
+                </label>
+                <input
+                  type="number"
+                  class="form-control"
+                  name="quantity"
+                  defaultValue={product.quantity}
+                />
+              </div>
+              <div class="mb-3">
+                <label for="InputPrice" class="form-label">
+                  Price
+                </label>
+                <input
+                  type="number"
+                  class="form-control"
+                  name="price"
+                  defaultValue={product.price}
+                />
+              </div>
+              {product.features.map((feature, index) => {
+                return (
+                  <div class="mb-3">
+                    <label for="InputFeatures" class="form-label">
+                      Features
+                    </label>
+                    <textarea
+                      class="form-control"
+                      name="features"
+                      rows="3"
+                      defaultValue={product.features[index]}
+                    ></textarea>
+                  </div>
+                );
+              })}
+
+              <div class="mb-3">
+                <label for="InputImageURL" class="form-label">
+                  Image URL
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  name="image"
+                  defaultValue={product.image}
+                />
+              </div>
+              <button type="submit" class="btn btn-primary">
+                Update
+              </button>
+            </form>
+          </div>
+        </body>
+      </html>
+    );
   }
 }
+
+module.exports = Edit;
