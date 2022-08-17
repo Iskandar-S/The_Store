909be2a9dbd77dc2861d2e33f86ff5d5170a4da5
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-08-17 06:25:51 -0400

    added navbar and form

--- views/New.jsx
+++ views/New.jsx
@@ -3,25 +3,145 @@ const React = require('react');
 class New extends React.Component {
   render() {
     return (
-      <html>
+      <html lang="en">
         <head>
           <meta charset="UTF-8" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          <title>New Product</title>
+          <title>Add Product</title>
+          <script
+            src="https://kit.fontawesome.com/2f4558e6ee.js"
+            crossorigin="anonymous"
+          ></script>
+          <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+            crossorigin="anonymous"
+          ></link>
           <link rel="stylesheet" href="/css/app.css" />
         </head>
         <body>
-          <form action="/pokemon" method="POST">
-            Product name: <input type="text" name="name" />
-            <br />
-            Image url: <input type="text" name="img" />
-            <br />
-            <input type="submit" name="" value="Create Product" />
-          </form>
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="/products">
+                <img
+                  src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                  alt=""
+                  width="90"
+                  height="24"
+                  class="d-inline-block align-text-top"
+                />
+                <span> but from </span>
+                <img
+                  src="https://1000logos.net/wp-content/uploads/2021/05/Wish-logo.png"
+                  alt=""
+                  width="85"
+                  height="24"
+                  class="d-inline-block align-text-top"
+                />
+              </a>
+            </div>
+          </nav>
+          <div class="container mt-5">
+            <form action="/products" method="POST">
+              <div class="mb-3">
+                <label for="InputProductTitle" class="form-label">
+                  Product title
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  name="productTitle"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="InputManufacturer" class="form-label">
+                  Manufacturer/Brand
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  name="manufacturer"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="InputQuantity" class="form-label">
+                  Quantity
+                </label>
+                <input
+                  type="number"
+                  class="form-control"
+                  name="quantity"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="InputPrice" class="form-label">
+                  Price
+                </label>
+                <input
+                  type="number"
+                  class="form-control"
+                  name="price"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="InputFeatures" class="form-label">
+                  Features
+                </label>
+                <textarea
+                  class="form-control"
+                  name="features"
+                  rows="3"
+                ></textarea>
+              </div>
+              <div class="mb-3">
+                <label for="InputFeatures" class="form-label">
+                  Features
+                </label>
+                <textarea
+                  class="form-control"
+                  name="features"
+                  rows="3"
+                ></textarea>
+              </div>
+              <div class="mb-3">
+                <label for="InputFeatures" class="form-label">
+                  Features
+                </label>
+                <textarea
+                  class="form-control"
+                  name="features"
+                  rows="3"
+                ></textarea>
+              </div>
+              <div class="mb-3">
+                <label for="InputFeatures" class="form-label">
+                  Features
+                </label>
+                <textarea
+                  class="form-control"
+                  name="features"
+                  rows="3"
+                ></textarea>
+              </div>
+              <div class="mb-3">
+                <label for="InputImageURL" class="form-label">
+                  Image URL
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  name="image"
+                />
+              </div>
+              <button type="submit" class="btn btn-primary">
+                Add
+              </button>
+            </form>
+          </div>
         </body>
       </html>
     );
